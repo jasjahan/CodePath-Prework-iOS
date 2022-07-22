bf6a019dcfff8106ccd8219525dca5b6704f36da
jasjahan <mjahan@mail.sfsu.edu>
2022-07-22 14:39:07 -0700

    Created a Settings View Controller

--- Prework_CodePath/ViewController.swift
+++ Prework_CodePath/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "Tip Calculator"
         // Do any additional setup after loading the view.
     }
 
